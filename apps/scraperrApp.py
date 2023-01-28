af65b28b3af87664eb7b4bc69a2cb81eaa5a0941
Shamim Mahbub <60071360+shamim237@users.noreply.github.com>
2023-01-28 13:38:09 +0600

    Create scraperrApp.py

---
+++ apps/scraperrApp.py
@@ -0,0 +1,27 @@
+import re
+import streamlit as st
+import nlpaug.augmenter.word as naw
+
+
+
+@st.cache(allow_output_mutation=True, ttl=48*3600)
+def load_model():
+    aug = naw.ContextualWordEmbsAug(
+        model_path='bert-base-uncased', action="insert")
+    return aug
+
+aug = load_model()
+
+def parphrase(passage):
+    sen = []
+    for i in passage:
+        res = len(re.findall(r'\w+', i))
+        if res == 2:
+            pass
+        else:
+            res = i.replace('"', "'").replace("\n", "")
+            sen.append(res)
+
+    pas = " ".join(sen)
+    para_text = aug.augment(pas)
+    return para_text
