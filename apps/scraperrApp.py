e5034ad6b81101f23665232f63c71b49fcaab951
Shamim Mahbub <60071360+shamim237@users.noreply.github.com>
2023-01-28 13:45:51 +0600

    Update scraperrApp.py

--- apps/scraperrApp.py
+++ apps/scraperrApp.py
@@ -1,27 +1,24 @@
 import re
 import streamlit as st
-import nlpaug.augmenter.word as naw
+from scrap import extract
 
+def app():
+    st.title('Scraper')
+    st.write('Please provide a link or copy and paste this link- https://www.amazon.com/dp/B09B9TB61G')
+    user_input = st.text_input('Enter website link','')
+    with st.spinner('Please wait...'):
+        if st.button('Scrape'):
+            data = extract(user_input)
+            sen = []
+            for i in data:
+                res = len(re.findall(r'\w+', i))
+                if res == 2:
+                    pass
+                else:
+                    res = i.replace('"', "'").replace("\n", "")
+                    sen.append(res)
 
-
-@st.cache(allow_output_mutation=True, ttl=48*3600)
-def load_model():
-    aug = naw.ContextualWordEmbsAug(
-        model_path='bert-base-uncased', action="insert")
-    return aug
-
-aug = load_model()
-
-def parphrase(passage):
-    sen = []
-    for i in passage:
-        res = len(re.findall(r'\w+', i))
-        if res == 2:
-            pass
-        else:
-            res = i.replace('"', "'").replace("\n", "")
-            sen.append(res)
-
-    pas = " ".join(sen)
-    para_text = aug.augment(pas)
-    return para_text
+            pas = " ".join(sen)   
+            st.write("Scrapted data: ")
+            st.write(pas)
+            st.success("Done!")
